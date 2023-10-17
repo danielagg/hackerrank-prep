e9eb4274e30691d59568c3e9815e57a33af5ae59
Daniel Agg <daniel.agg@outlook.com>
2023-10-17 20:47:23 +0200

    plusMinus solution

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+plusMinus(new[] { -4, 3, -9, 0, 4, 1 }.ToList());
+
+void plusMinus(List<int> arr)
+{
+    var zeros = arr.Count(x => x == 0) / (float)arr.Count;
+    var positives = arr.Count(x => x > 0) / (float)arr.Count;
+    var negatives = arr.Count(x => x < 0) / (float)arr.Count;
+
+    Console.WriteLine(positives.ToString("0.000000"));
+    Console.WriteLine(negatives.ToString("0.000000"));
+    Console.WriteLine(zeros.ToString("0.000000"));
+}
